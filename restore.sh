00b92418a0c1ab0d4d5ada4c9efb6c135264a665
Lukas Schneider <lukas.schneider@cmd.nrw>
2026-04-14 11:57:30 +0200

    fix: fixed a bug in restore.sh

--- restore.sh
+++ restore.sh
@@ -1,5 +1,8 @@
-cp -r ~/personal/dotfiles/.config ~/.config/nvim 
+cp -r ~/personal/dotfiles/.config/nvim ~/.config
+cp -r ~/personal/dotfiles/.config/omarchy ~/.config
+cp -r ~/personal/dotfiles/.config/tmux ~/.config
+cp ~/personal/dotfiles/.config/starship.toml ~/.config
 
 cp -r ~/personal/dotfiles/.local/bin ~/.local/bin/tmux-sessionizer 
 
-cp -r ~/personal/dotfiles/.zshrc ~/
+cp ~/personal/dotfiles/.zshrc ~/
