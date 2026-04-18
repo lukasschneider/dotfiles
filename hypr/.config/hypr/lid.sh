7ab3e4abb03e64f827a78eee52abbce9bd17301d
Lukas Schneider <schneiderlukas197@gmail.com>
2026-04-18 17:40:37 +0200

    added config for lid

---
+++ hypr/.config/hypr/lid.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+EXTERNAL="DP-3"
+INTERNAL="eDP-1"
+
+if hyprctl monitors | grep -q "$EXTERNAL"; then
+    # Externer Monitor ist angeschlossen
+    if [ "$1" = "close" ]; then
+        # Lid zu -> internen Monitor deaktivieren
+        hyprctl keyword monitor "$INTERNAL, disable"
+    else
+        # Lid offen -> internen Monitor links vom externen platzieren
+        hyprctl keyword monitor "$INTERNAL, preferred, -1920x0, 1"
+    fi
+else
+    # Kein externer Monitor -> interner IMMER aktiv
+    hyprctl keyword monitor "$INTERNAL, preferred, auto, 1"
+fi
